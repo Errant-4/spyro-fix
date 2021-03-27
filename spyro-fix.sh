d155370ad3c477d4292a5fe2522e78a617cbe0e8
dmnct <35878406+dmnct@users.noreply.github.com>
2021-03-27 14:07:23 +0000

    Update spyro-fix.sh

--- spyro-fix.sh
+++ spyro-fix.sh
@@ -24,7 +24,7 @@ mkdir "$targetdir"
 for file in "${backupdir}"*
 do
 	if mediainfo --output=XML "$file" | grep -q "<ID>4</ID>"; then
-		ffmpeg -loglevel error -stats -i "$file" -map 0:a:"$language" -map 0:v:0 -c:a copy "${file/$backupdir/$targetdir}"
+		ffmpeg -loglevel error -stats -i "$file" -map 0:a:"$language" -map 0:v:0 -c:a copy -c:v copy "${file/$backupdir/$targetdir}"
 	else 
 		cp "$file" "${file/$backupdir/$targetdir}"
 	fi
